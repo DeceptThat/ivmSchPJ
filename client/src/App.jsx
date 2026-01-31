e74dafcb554354feeb3c087536f26bb9d1dd57e1
Elcon <yamizenkoku@gmail.com>
2026-01-31 17:53:23 +0700

    Setup React and Node Hello World

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello World!</h1>
+      <p>My React app is running on localhost:5173</p>
+    </div>
+  );
+}
+
+export default App;
